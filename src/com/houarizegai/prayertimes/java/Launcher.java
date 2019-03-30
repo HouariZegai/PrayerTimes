c1708e958e29acb0a34a2d4fd23f90fba03250a3
HouariZEGAI <houarizegai14@gmail.com>
2019-03-30 15:23:59 +0100

    Making app icon

--- src/com/houarizegai/prayertimes/java/Launcher.java
+++ src/com/houarizegai/prayertimes/java/Launcher.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -14,6 +15,8 @@ public class Launcher extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/com/houarizegai/prayertimes/resources/views/PrayerTimes.fxml"));
         stage.setScene(new Scene(root));
+        // Make icon to the app
+        stage.getIcons().add(new Image("/com/houarizegai/prayertimes/resources/images/icon-app-64px.png"));
         stage.initStyle(StageStyle.UNDECORATED);
         Launcher.stage = stage;
         stage.show();
